b7ad091aefea1a8c9e7a613a5b8dec1810faed09
pumbas600 <joshjeffers600@gmail.com>
2023-04-01 01:29:20 +1300

    fix: :bug: Add missing prop for ContributionsChart in index file

--- src/pages/api/contributions/[username].tsx
+++ src/pages/api/contributions/[username].tsx
@@ -6,7 +6,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { renderToString } from 'react-dom/server';
 import { ZodError } from 'zod';
 
-const DefaultOptions: Options = {
+export const DefaultOptions: Options = {
     color: '5bcdec',
     width: 1200,
     height: 450,
@@ -25,7 +25,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             // Remove surrounding <div></div>
             const htmlWithoutDiv = html.substring(html.indexOf('>') + 1, html.lastIndexOf('<'));
 
-            svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${options.width}" height="${options.height}" viewBox="0 0 ${options.width} ${options.height}"><style>svg { background-color: #0d1117; } </style>${htmlWithoutDiv}</svg>`;
+            svg = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="${options.width}"
+                        height="${options.height}" viewBox="0 0 ${options.width} ${options.height}">
+                    <style>svg { background-color: #0d1117; }</style>
+                    ${htmlWithoutDiv}
+                </svg>`;
+
             cacheSvg(username, options, svg);
         }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import ContributionsChart from '@/components/ContributionsChart';
+import { DefaultOptions } from './api/contributions/[username]';
 
 export default function Home() {
     return (
@@ -11,7 +12,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-                <ContributionsChart />
+                <ContributionsChart options={DefaultOptions} />
             </main>
         </>
     );
