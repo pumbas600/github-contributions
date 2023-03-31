95f4214f096bae5c2f7f5903da2a4a7aa97890b5
pumbas600 <joshjeffers600@gmail.com>
2023-04-01 00:30:12 +1300

    feat: :sparkles: Style the chart and add a title + axis labels

--- src/components/ContributionsChart.tsx
+++ src/components/ContributionsChart.tsx
@@ -5,6 +5,19 @@ interface Contribution {
     count: number;
 }
 
+const LabelStyles = {
+    fill: '#5bcdec',
+    fontSize: 20,
+    fontWeight: 700,
+};
+
+const AxisStyles = {
+    tick: { fill: '#5bcdec' },
+};
+
+// You don't seem to be able to directly assign this to a react component
+const htmlXmlNs = { xmlns: 'http://www.w3.org/1999/xhtml' };
+
 export default function ContributionsChart() {
     const contributions: Contribution[] = [
         {
@@ -50,11 +63,44 @@ export default function ContributionsChart() {
     ];
 
     return (
-        <LineChart width={900} height={400} data={contributions}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="day" />
-            <YAxis />
-            <Line type="monotone" dataKey="count" stroke="#82ca9d" />
+        <LineChart
+            width={1200}
+            height={450}
+            data={contributions}
+            margin={{
+                top: 80,
+                right: 20,
+                bottom: 30,
+                left: 30,
+            }}
+            style={{
+                backgroundColor: '#0d1117',
+            }}
+        >
+            <text x={1200 / 2 + 30} y={32} fill="#5bcdec" textAnchor="middle" dominantBaseline="central">
+                <tspan fontSize={32} fontWeight={800}>
+                    {`pumbas600's Contributions`}
+                </tspan>
+            </text>
+            <CartesianGrid strokeDasharray="3 3" stroke="#5bcdec" strokeOpacity={0.3} />
+            <XAxis dataKey="day" label={{ value: 'Day', dy: 15, ...LabelStyles }} {...AxisStyles} />
+            <YAxis
+                label={{
+                    value: 'Contributions',
+                    angle: -90,
+                    dx: -15,
+                    ...LabelStyles,
+                }}
+                {...AxisStyles}
+            />
+            <Line
+                type="monotone"
+                dataKey="count"
+                stroke="#5bcdec"
+                strokeWidth={4}
+                fill="#5bcdec"
+                dot={{ fill: 'white', stroke: 'white' }}
+            />
         </LineChart>
     );
 }
--- src/pages/api/contributions/[username].tsx
+++ src/pages/api/contributions/[username].tsx
@@ -1,15 +1,16 @@
 import ContributionsChart from '@/components/ContributionsChart';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { renderToPipeableStream, renderToString } from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 
 let cachedSvg: string | null = null;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (!cachedSvg) {
         const html = renderToString(<ContributionsChart />);
-        cachedSvg = html
-            .substring(html.indexOf('>') + 1, html.lastIndexOf('<'))
-            .replace('<svg', '<svg xmlns="http://www.w3.org/2000/svg"');
+
+        const htmlWithoutDiv = html.substring(html.indexOf('>') + 1, html.lastIndexOf('<'));
+
+        cachedSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="1200" height="450" viewBox="0 0 1200 420"><style>svg { background-color: #0d1117; } </style>${htmlWithoutDiv}</svg>`; // Remove surrounding <div></div>
     }
 
     res.status(200).setHeader('Content-Type', 'image/svg+xml').send(cachedSvg);
