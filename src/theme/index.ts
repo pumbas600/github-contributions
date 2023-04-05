473a3e44c954b187f071f1f34d92f5ff99367aa3
pumbas600 <joshjeffers600@gmail.com>
2023-04-05 22:09:22 +1200

    style: :lipstick: Update theme colours

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,8 +2,18 @@ import { createTheme } from '@mui/material';
 
 export const theme = createTheme({
     palette: {
+        primary: {
+            main: '#30B0C7',
+            light: '#C0E7EE',
+            contrastText: '#FFFFFF',
+        },
+        secondary: {
+            main: '#FF9500',
+            light: 'FFDFB2',
+        },
         background: {
-            default: '',
+            default: '#F2F2F7',
+            paper: '#FFFFFF',
         },
     },
     components: {
