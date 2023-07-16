6c4d9c9113e82be561e4516f61c85c1b640cb0f9
pumbas600 <joshjeffers600@gmail.com>
2023-07-16 18:23:18 +1200

    refactor: :recycle: Remove caching entirely

--- src/pages/api/contributions/[username].tsx
+++ src/pages/api/contributions/[username].tsx
@@ -1,4 +1,3 @@
-import { cacheSvg, getCachedSvg } from '@/cache';
 import ContributionsChart from '@/components/ContributionsChart';
 import { QueryParamsModel } from '@/models/QueryParams';
 import { ContributionsService } from '@/services/ContributionsService';
@@ -12,43 +11,39 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const { username, ...queryOptions } = await QueryParamsModel.parseAsync(req.query);
         const options = OptionsService.getOptions(queryOptions);
 
-        let svg = null; // getCachedSvg(username, options);
-        if (!svg) {
-            const contributions = await ContributionsService.getContributions(username, options.from, options.to);
+        const contributions = await ContributionsService.getContributions(username, options.from, options.to);
 
-            const start = Date.now();
-            const html = renderToString(
-                <ContributionsChart username={username} options={options} contributions={contributions} />,
-            );
-            console.log(`Rendered chart in ${Date.now() - start}ms`);
+        const start = Date.now();
+        const html = renderToString(
+            <ContributionsChart username={username} options={options} contributions={contributions} />,
+        );
 
-            // Remove surrounding <div></div>
-            const htmlWithoutDiv = html.substring(html.indexOf('>') + 1, html.lastIndexOf('<'));
+        console.log(`Rendered chart in ${Date.now() - start}ms`);
 
-            svg = `
-                <svg xmlns="http://www.w3.org/2000/svg" width="${options.width}"
-                     height="${options.height}" viewBox="0 0 ${options.width} ${options.height}">
-                    <style>
-                        svg {
-                            background-color: ${options.bgColour};
-                            font-family: Segoe UI,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,Arial;
-                        }
-                        .recharts-line > path {
-                            animation: draw 5s ease-in-out forwards;
-                            stroke-dasharray: 5000;
-                            stroke-dashoffset: 5000;
-                        }
+        // Remove surrounding <div></div>
+        const htmlWithoutDiv = html.substring(html.indexOf('>') + 1, html.lastIndexOf('<'));
 
-                        @keyframes draw {
-                            to { stroke-dashoffset: 0; }
-                        }
+        const svg = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="${options.width}"
+                    height="${options.height}" viewBox="0 0 ${options.width} ${options.height}">
+                <style>
+                    svg {
+                        background-color: ${options.bgColour};
+                        font-family: Segoe UI,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,Arial;
+                    }
+                    .recharts-line > path {
+                        animation: draw 5s ease-in-out forwards;
+                        stroke-dasharray: 5000;
+                        stroke-dashoffset: 5000;
+                    }
 
-                    </style>
-                    ${htmlWithoutDiv}
-                </svg>`;
+                    @keyframes draw {
+                        to { stroke-dashoffset: 0; }
+                    }
 
-            cacheSvg(username, options, svg);
-        }
+                </style>
+                ${htmlWithoutDiv}
+            </svg>`;
 
         res.status(200).setHeader('Content-Type', 'image/svg+xml').send(svg);
     } catch (error) {
