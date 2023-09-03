9b94cd870e26c8bf37ed743cb557189a4efef5f5
pumbas600 <joshjeffers600@gmail.com>
2023-09-03 18:03:00 +1200

    feat: :sparkles: Add method for logging cached and uncached metrics

--- src/services/MetricsService.ts
+++ src/services/MetricsService.ts
@@ -1,19 +1,38 @@
 import { db } from '@/firebase';
-import ContributionMetric from '@/types/interfaces/ContributionMetric';
+import ContributionMetric, {
+    CachedContributionMetric,
+    UncachedContributionMetric,
+} from '@/types/interfaces/ContributionMetric';
 import { addDoc, collection } from 'firebase/firestore';
 
 export namespace MetricsService {
     const githubContributionMetrics = collection(db, 'github_contribution_metrics');
 
     export async function logContributionsRequest(
-        partialMetric: Omit<ContributionMetric, 'date' | 'environment'>,
+        partialMetric: Omit<UncachedContributionMetric, 'date' | 'environment' | 'cacheHit'>,
     ): Promise<void> {
-        const metric: ContributionMetric = {
+        const metric: UncachedContributionMetric = {
             ...partialMetric,
+            cacheHit: false,
             date: new Date(),
             environment: process.env.NODE_ENV,
         };
 
+        logMetric(metric);
+    }
+
+    export async function logCachedContributionsRequest(username: string): Promise<void> {
+        const metric: CachedContributionMetric = {
+            username,
+            cacheHit: true,
+            date: new Date(),
+            environment: process.env.NODE_ENV,
+        };
+
+        logMetric(metric);
+    }
+
+    async function logMetric(metric: ContributionMetric) {
         const docRef = await addDoc(githubContributionMetrics, metric);
         console.log(`Logged contributions request for user '${metric.username}' with id: '${docRef.id}'.`);
     }
--- src/types/interfaces/ContributionMetric.ts
+++ src/types/interfaces/ContributionMetric.ts
@@ -1,7 +1,18 @@
-export default interface ContributionMetric {
+export interface CommonContributionMetric {
     username: string;
     date: Date;
     environment: string;
+}
+
+export interface CachedContributionMetric extends CommonContributionMetric {
+    cacheHit: true;
+}
+
+export interface UncachedContributionMetric extends CommonContributionMetric {
+    cacheHit: false;
     fetchingMs: number;
     chartRenderingMs: number;
 }
+
+type ContributionMetric = CachedContributionMetric | UncachedContributionMetric;
+export default ContributionMetric;
