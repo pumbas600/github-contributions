f60c09d9414cfa587c80be7be46215e1bd4d3f84
pumbas600 <joshjeffers600@gmail.com>
2023-09-21 23:29:53 +1200

    feat: :sparkles: Add a theme menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,9 @@
-import { IconButton, styled } from '@mui/material';
+import { IconButton, Stack, styled } from '@mui/material';
 import Title from '../typography/Title';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import Link from 'next/link';
-import { GitHubRepoUrl } from '@/data/links';
+import { GitHubRepoUrl } from '@/data/Links';
+import ThemeMenu from '../menus/ThemeMenu';
 
 const SpacedHeader = styled('header')(({ theme }) => ({
     display: 'flex',
@@ -16,11 +17,14 @@ export default function Header() {
     return (
         <SpacedHeader>
             <Title />
-            <Link href={GitHubRepoUrl}>
-                <IconButton aria-label="View the source code">
-                    <GitHubIcon />
-                </IconButton>
-            </Link>
+            <Stack direction="row" gap={1}>
+                <Link href={GitHubRepoUrl}>
+                    <IconButton aria-label="View the source code">
+                        <GitHubIcon />
+                    </IconButton>
+                </Link>
+                <ThemeMenu />
+            </Stack>
         </SpacedHeader>
     );
 }
---
+++ src/components/menus/ThemeMenu.tsx
@@ -0,0 +1,54 @@
+import { Box, Button, IconButton, ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material';
+import { useState, MouseEvent } from 'react';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+
+export default function ThemeMenu() {
+    const [anchorElement, setAnchorElement] = useState<null | HTMLElement>(null);
+    const isOpen = anchorElement !== null;
+
+    const handleOpen = (event: MouseEvent<HTMLButtonElement>) => {
+        setAnchorElement(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorElement(null);
+    };
+
+    return (
+        <Box>
+            <IconButton
+                id="theme-button"
+                aria-controls={isOpen ? 'theme-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={isOpen ? 'true' : undefined}
+                aria-label="Change theme"
+                onClick={handleOpen}
+            >
+                <LightModeIcon />
+            </IconButton>
+            <Menu
+                id="theme-menu"
+                anchorEl={anchorElement}
+                open={isOpen}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'theme-button',
+                }}
+            >
+                <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <LightModeIcon color="primary" />
+                    </ListItemIcon>
+                    <ListItemText>Light</ListItemText>
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                        <DarkModeOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText>Dark</ListItemText>
+                </MenuItem>
+            </Menu>
+        </Box>
+    );
+}
