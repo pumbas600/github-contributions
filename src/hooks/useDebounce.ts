da7c5fa7aeca355c49455ffa1fe709736d588f0a
pumbas600 <joshjeffers600@gmail.com>
2023-09-21 20:50:52 +1200

    refactor: :recycle: Cleanup useDebounce hook

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,20 +1,14 @@
 import { useEffect, useState } from 'react';
 
 // Taken from: https://usehooks.com/useDebounce/
-export default function useDebounce<T>(value: T, delay: number): T {
-    // State and setters for debounced value
+export default function useDebounce<T>(value: T, delay = 500): T {
     const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
     useEffect(() => {
-        // Update debounced value after delay
-        const handler = setTimeout(() => {
-            setDebouncedValue(value);
-        }, delay);
-        // Cancel the timeout if value changes (also on delay change or unmount)
-        // This is how we prevent debounced value from updating if value is changed ...
-        // .. within the delay period. Timeout gets cleared and restarted.
-        return () => {
-            clearTimeout(handler);
-        };
+        const handler = setTimeout(() => setDebouncedValue(value), delay);
+
+        return () => clearTimeout(handler);
     }, [value, delay]);
+
     return debouncedValue;
 }
