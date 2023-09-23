e9fcfd7bf75ee4b3ab2c29998480bbcebc85f49d
pumbas600 <joshjeffers600@gmail.com>
2023-09-23 14:56:45 +1200

    style: :lipstick: Make loading indicator appear over the top of the graph

--- src/components/forms/ChartImage.tsx
+++ src/components/forms/ChartImage.tsx
@@ -1,20 +1,31 @@
 import { Box, CircularProgress, Paper, Stack, Typography, styled } from '@mui/material';
 import { useEffect, useState } from 'react';
 
-/**
- * Restrict the max height of the image in case the user specifies a large height or
- * the chart width is small, which would allow the chart's height to be very large as
- * it attempts to fill the width and therefore, scale vertically to match.
- */
-const Img = styled('img')({
-    maxHeight: '400px',
-});
-
 export interface ChartImageProps {
     src: string;
     alt: string;
     onError?: () => void;
 }
+
+/**
+ * Restrict the max height of the image in case the user specifies a large height or
+ * the chart width is small, which would allow the chart's height to be very large as
+ * it attempts to fill the width and therefore, scale vertically to match.
+ */
+const ChartContainer = styled(Box)(({ theme }) => ({
+    position: 'relative',
+    display: 'inline-flex',
+    alignItems: 'center',
+    maxHeight: '400px',
+    minHeight: '100px',
+}));
+
+const LoadingContainer = styled(Stack)(({ theme }) => ({
+    position: 'absolute',
+    gap: theme.spacing(1),
+    alignItems: 'center',
+    width: '100%',
+}));
 
 export default function ChartImage({ src, alt, onError }: ChartImageProps) {
     const [isLoading, setIsLoading] = useState(true);
@@ -24,14 +35,14 @@ export default function ChartImage({ src, alt, onError }: ChartImageProps) {
     }, [src]);
 
     return (
-        <>
-            <Img src={src} alt={alt} onError={onError} onLoad={() => setIsLoading(false)} />
+        <ChartContainer>
+            <img src={src} alt={alt} onError={onError} onLoad={() => setIsLoading(false)} width="100%" height="100%" />
             {isLoading && (
-                <Stack width="100%" alignItems="center" gap={1}>
+                <LoadingContainer>
                     <CircularProgress />
                     <Typography>Generating GitHub Contributions Graph...</Typography>
-                </Stack>
+                </LoadingContainer>
             )}
-        </>
+        </ChartContainer>
     );
 }
