1a07a05e824da196cd2197a88322977d84f74ee0
pumbas600 <joshjeffers600@gmail.com>
2023-09-23 14:19:39 +1200

    refactor: :recycle: Make checkbox labels not clickable too

--- src/components/forms/LabelledCheckbox.tsx
+++ src/components/forms/LabelledCheckbox.tsx
@@ -1,14 +1,14 @@
-import { Box, Checkbox, CheckboxProps, FormControlLabel } from '@mui/material';
+import { Checkbox, CheckboxProps, Stack, Typography } from '@mui/material';
 
 interface LabelledCheckboxProps extends CheckboxProps {
     label: string;
 }
 
 export default function LabelledCheckbox({ label, ...props }: LabelledCheckboxProps) {
-    // Apply the 100% to the Box instead of the FormControlLabel so that the empty space is not also clickable.
     return (
-        <Box sx={{ width: '100%' }}>
-            <FormControlLabel label={label} control={<Checkbox {...props} />} />
-        </Box>
+        <Stack sx={{ width: '100%' }} direction="row" alignItems="center" gap={0.5}>
+            <Checkbox {...props} />
+            <Typography>{label}</Typography>
+        </Stack>
     );
 }
