7638aa229dacd21fdfe793111f97604414334e15
pumbas600 <joshjeffers600@gmail.com>
2023-09-23 13:54:23 +1200

    fix: :bug: Whitespace to the right of checkboxes being clickable

--- src/components/forms/LabelledCheckbox.tsx
+++ src/components/forms/LabelledCheckbox.tsx
@@ -1,9 +1,14 @@
-import { Checkbox, CheckboxProps, FormControlLabel } from '@mui/material';
+import { Box, Checkbox, CheckboxProps, FormControlLabel } from '@mui/material';
 
 interface LabelledCheckboxProps extends CheckboxProps {
     label: string;
 }
 
 export default function LabelledCheckbox({ label, ...props }: LabelledCheckboxProps) {
-    return <FormControlLabel sx={{ width: '100%' }} label={label} control={<Checkbox {...props} />} />;
+    // Apply the 100% to the Box instead of the FormControlLabel so that the empty space is not also clickable.
+    return (
+        <Box sx={{ width: '100%' }}>
+            <FormControlLabel label={label} control={<Checkbox {...props} />} />
+        </Box>
+    );
 }
