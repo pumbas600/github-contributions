a8c8cd158bde9790372a90dcbeba627993229c26
pumbas600 <joshjeffers600@gmail.com>
2023-10-28 12:55:21 +1300

    feat: :sparkles: Handle errors in the metric endpoints

--- src/pages/api/metrics.ts
+++ src/pages/api/metrics.ts
@@ -1,14 +1,19 @@
 import { MetricsModel } from '@/models/MetricsModel';
+import { ErrorService } from '@/services/ErrorService';
 import { MetricsService } from '@/services/MetricsService';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-    const { since } = await MetricsModel.parseAsync(req.query);
+    try {
+        const { since } = await MetricsModel.parseAsync(req.query);
 
-    const count =
-        since === undefined
-            ? await MetricsService.getGeneratedGraphCount()
-            : await MetricsService.getGeneratedGraphCountSince(since);
+        const count =
+            since === undefined
+                ? await MetricsService.getGeneratedGraphCount()
+                : await MetricsService.getGeneratedGraphCountSince(since);
 
-    res.status(200).json({ count });
+        res.status(200).json({ count });
+    } catch (error) {
+        ErrorService.handleError(res, error);
+    }
 }
--- src/pages/api/metrics/[username].ts
+++ src/pages/api/metrics/[username].ts
@@ -1,14 +1,19 @@
 import { MetricsWithUsernameModel } from '@/models/MetricsModel';
+import { ErrorService } from '@/services/ErrorService';
 import { MetricsService } from '@/services/MetricsService';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-    const { username, since } = await MetricsWithUsernameModel.parseAsync(req.query);
+    try {
+        const { username, since } = await MetricsWithUsernameModel.parseAsync(req.query);
 
-    const count =
-        since === undefined
-            ? await MetricsService.getGeneratedGraphCountForUser(username)
-            : await MetricsService.getGeneratedGraphCountForUserSince(username, since);
+        const count =
+            since === undefined
+                ? await MetricsService.getGeneratedGraphCountForUser(username)
+                : await MetricsService.getGeneratedGraphCountForUserSince(username, since);
 
-    res.status(200).json({ count });
+        res.status(200).json({ count });
+    } catch (error) {
+        ErrorService.handleError(res, error);
+    }
 }
