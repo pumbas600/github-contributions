8eadb1c4dc41990d6e6616b50107707bab763423
pumbas600 <joshjeffers600@gmail.com>
2023-10-28 12:30:29 +1300

    feat: :sparkles: Add functions for getting metric counts

--- src/services/MetricsService.ts
+++ src/services/MetricsService.ts
@@ -1,6 +1,6 @@
 import { db } from '@/firebase';
 import ContributionMetric from '@/types/interfaces/ContributionMetric';
-import { addDoc, collection } from 'firebase/firestore';
+import { Query, addDoc, collection, getCountFromServer, query, where } from 'firebase/firestore';
 
 export namespace MetricsService {
     const githubContributionMetrics = collection(db, 'github_contribution_metrics');
@@ -16,5 +16,40 @@ export namespace MetricsService {
 
         const docRef = await addDoc(githubContributionMetrics, metric);
         console.log(`Logged contributions request for user '${metric.username}' with id: '${docRef.id}'.`);
+    }
+
+    export async function getGeneratedGraphCount(source: Query = githubContributionMetrics): Promise<number> {
+        try {
+            const response = await getCountFromServer(source);
+            return response.data().count;
+        } catch (error) {
+            console.error(error);
+            return -1;
+        }
+    }
+
+    export async function getGeneratedGraphCountSince(
+        since: Date,
+        source = githubContributionMetrics,
+    ): Promise<number> {
+        const sinceQuery = query(source, where('date', '>=', since));
+        return getGeneratedGraphCount(sinceQuery);
+    }
+
+    export async function getGeneratedGraphCountForUser(
+        username: string,
+        source: Query = githubContributionMetrics,
+    ): Promise<number> {
+        const userQuery = query(source, where('username', '==', username));
+        return getGeneratedGraphCount(userQuery);
+    }
+
+    export async function getGeneratedGraphCountForUserSince(
+        username: string,
+        since: Date,
+        source = githubContributionMetrics,
+    ): Promise<number> {
+        const sinceQuery = query(source, where('date', '>=', since));
+        return getGeneratedGraphCountForUser(username, sinceQuery);
     }
 }
