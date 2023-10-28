3b86e21cdd71d8a91952d9045b2debc5ecc5b2de
pumbas600 <joshjeffers600@gmail.com>
2023-10-29 12:07:12 +1300

    feat: :sparkles: Respond to `OPTIONS` requests in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,10 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(): NextResponse {
-    const res = NextResponse.next();
+export function middleware(req: NextRequest): Response {
+    const res = req.method === 'OPTIONS' ? new Response(undefined, { status: 204 }) : NextResponse.next();
 
     res.headers.append('Access-Control-Allow-Origin', '*');
     res.headers.append('Access-Control-Allow-Methods', 'GET');
-
     return res;
 }
 
