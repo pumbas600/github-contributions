0c5e53d5c7ffbd98f6f9a4012330111c6585ac96
pumbas600 <joshjeffers600@gmail.com>
2025-09-13 13:20:23 +1200

    feat: :sparkles: add shrink rect helper

--- src/svg-rendering/rect.ts
+++ src/svg-rendering/rect.ts
@@ -17,3 +17,29 @@ export function rectFromPoints(origin: Point, endPoint: Point): Rect {
         height: endPoint.y - origin.y,
     };
 }
+
+export interface ShrinkOptions {
+    xStart?: number;
+    xEnd?: number;
+    yStart?: number;
+    yEnd?: number;
+    x?: number;
+    y?: number;
+}
+
+export function shrinkRect(rect: Rect, options: ShrinkOptions | number): Rect {
+    if (typeof options === 'number') {
+        options = { x: options, y: options };
+    }
+
+    const xStart = options.xStart ?? options.x ?? 0;
+    const xEnd = options.xEnd ?? options.x ?? 0;
+    const yStart = options.yStart ?? options.y ?? 0;
+    const yEnd = options.yEnd ?? options.y ?? 0;
+
+    return {
+        origin: addPoints(rect.origin, { x: xStart, y: yStart }),
+        width: Math.max(0, rect.width - xStart - xEnd),
+        height: Math.max(0, rect.height - yStart - yEnd),
+    };
+}
